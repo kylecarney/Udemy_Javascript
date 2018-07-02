e0b7407e244906b444641a581ea8e4cf8353f1c7
Kyle Carney <kyle.e.carney@gmail.com>
2018-07-02 18:25:32 -0400

    udemy javascript course added

---
+++ The Complete JavaScript/complete-javascript-course-master/2-JS-basics/starter/script.js
@@ -0,0 +1,23 @@
+//var massMark = 78; //kg
+//var heightMark = 1.69; // meters
+//
+//var massJohn = 92; 
+//var heightJohn= 1.95; 
+//
+//var BMIMark = massMark / (heightMark * heightMark);
+//var BMIJohn = massJohn / (heightJohn * heightJohn);
+//
+//var markHigherBMI = BMIMark > BMIJohn;
+//console.log('Is Mark\'s BMI higher than John\'s ' + markHigherBMI);
+
+
+var firstName = 'John';
+var civilStatus = 'single';
+
+if(civilStatus === 'married'){
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon!')
+}
+
+var isMarried
